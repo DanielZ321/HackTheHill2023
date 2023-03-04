688702d2e9da2f240590570bc7399792123b25af
DanielZ321 <36174350+DanielZ321@users.noreply.github.com>
2023-03-04 16:05:39 -0500

    Testing123

--- 256chat/src/App.jsx
+++ 256chat/src/App.jsx
@@ -1,34 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import {firebase} from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import './App.css';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFaE2v0KsbQzkLdLvcJt5HGUkrsvD20lA",
+  authDomain: "chatapp-80847.firebaseapp.com",
+  projectId: "chatapp-80847",
+  storageBucket: "chatapp-80847.appspot.com",
+  messagingSenderId: "132817232542",
+  appId: "1:132817232542:web:387ffb22e4c3dca2a6118f",
+  measurementId: "G-Y31WESCL1C"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  const [newMessage, setNewMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+  
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+      const messagesRef = firebase.database().ref('messages');
+    messagesRef.on('child_added', (snapshot) => {
+      const message = snapshot.val();
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+  }, []);
+
+  const handleSignIn = () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider);
+  };
+
+  const handleSignOut = () => {
+       firebase.auth().signOut();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const messageObject = {
+      text: newMessage,
+      user: {
+        uid: user.uid,
+        displayName: user.displayName
+      },
+      createdAt: firebase.database.ServerValue.TIMESTAMP
+    };
+        firebase.database().ref('messages').push(messageObject);
+        setNewMessage('');
+  };
+
+  const handleInputChange = (e) => {
+    setNewMessage(e.target.value);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        <h1>Chat Room</h1>
+        {user ? (
+          <button onClick={handleSignOut}>Sign Out</button>
+        ) : (
+          <button onClick={handleSignIn}>Sign In with Google</button>
+        )}
+      </header>
+      <div className="container">
+        {user ? (
+          <div>
+            <form onSubmit={handleSubmit}>
+              <input type="text" value={newMessage} onChange={handleInputChange} placeholder="Type a message..." />
+              <button type="submit">Send</button>
+            </form>
+            <ul>
+              {messages.map((message) => (
+                <li key={message.createdAt}>
+                  <strong>{message.user.displayName}</strong>: {message.text}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>Please sign in to view and send messages.</p>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
