533cdea6ace0a7bc95e8f517de147cd43c9b19f0
KennyCXD <71290690+KennyCXD@users.noreply.github.com>
2023-03-04 15:26:56 -0500

    Test new Frontend

--- 256chat/src/App.jsx
+++ 256chat/src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState([]);
+
+  function handleSendMessage(message) {
+    setMessages([...messages, message]);
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="conversation">
+        <div className="messages">
+          {messages.map((message, index) => (
+            <div key={index} className="message">
+              <div className="username">{message.username}</div>
+              <div className="text">{message.text}</div>
+            </div>
+          ))}
+        </div>
+        <MessageInput onSendMessage={handleSendMessage} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+function MessageInput({ onSendMessage }) {
+  const [message, setMessage] = useState('');
+  const [username, setUsername] = useState('');
+
+  function handleInputChange(event) {
+    setMessage(event.target.value);
+  }
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    if (message) {
+      onSendMessage({ id: Date.now(), username: username, text: message });
+      setMessage('');
+    }
+  }
+
+  return (
+    <form onSubmit={handleFormSubmit} className="message-input">
+      <input type="text" value={username} onChange={handleUsernameChange} placeholder="Your username" />
+      <input type="text" value={message} onChange={handleInputChange} placeholder="Type a message..." />
+      <button type="submit">Send</button>
+    </form>
+  );
+}
+
+export default App;
