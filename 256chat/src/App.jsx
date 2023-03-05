d5fa1b35a2a12ba85042bd56a61d90df3962e9a6
KennyCXD <71290690+KennyCXD@users.noreply.github.com>
2023-03-04 19:50:13 -0500

    Fixed stuff

--- 256chat/src/App.jsx
+++ 256chat/src/App.jsx
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import {firebase} from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-const firebaseConfig = {
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, signOut } from 'firebase/auth';
+import { getDatabase, ref, push, onChildAdded, serverTimestamp } from 'firebase/database';
+import { GoogleAuthProvider } from 'firebase/auth';
+
+import * as qrcode from 'qrcode';
+import * as qrgenerator from 'qrcode-generator';
+
+const firebaseConfig = {  
   apiKey: "AIzaSyBFaE2v0KsbQzkLdLvcJt5HGUkrsvD20lA",
   authDomain: "chatapp-80847.firebaseapp.com",
   projectId: "chatapp-80847",
@@ -14,16 +19,21 @@ const firebaseConfig = {
   measurementId: "G-Y31WESCL1C"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
 
 function App() {
+  // Component state
   const [user, setUser] = useState(null);
   const [newMessage, setNewMessage] = useState('');
   const [messages, setMessages] = useState([]);
+  const [secret, setSecret] = useState(null);
+  const [qrCodeUrl, setQrCodeUrl] = useState(null);
 
   useEffect(() => {
-  
-    firebase.auth().onAuthStateChanged((user) => {
+    // Firebase auth state listener
+    const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         setUser(user);
       } else {
@@ -31,20 +41,39 @@ function App() {
       }
     });
 
-      const messagesRef = firebase.database().ref('messages');
-    messagesRef.on('child_added', (snapshot) => {
+    // Firebase database listener
+    const messagesRef = ref(database, 'messages');
+    const unsubscribeMessages = onChildAdded(messagesRef, (snapshot) => {
       const message = snapshot.val();
       setMessages((prevMessages) => [...prevMessages, message]);
     });
-  }, []);
 
+    // Cleanup
+    return () => {
+      unsubscribe();
+      unsubscribeMessages();
+    };
+  }, [auth, database]);
+
+  // Event handlers
   const handleSignIn = () => {
-      const provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider);
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider).then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const idToken = credential.idToken;
+      const qr = qrgenerator(4, 'M');
+      qr.addData(idToken);
+      qr.make();
+      const qrCodeUrl = qr.createDataURL();
+      setSecret(idToken);
+      setQrCodeUrl(qrCodeUrl);
+    }).catch((error) => {
+      console.log(error);
+    });
   };
 
   const handleSignOut = () => {
-       firebase.auth().signOut();
+    signOut(auth);
   };
 
   const handleSubmit = (e) => {
@@ -55,10 +84,10 @@ function App() {
         uid: user.uid,
         displayName: user.displayName
       },
-      createdAt: firebase.database.ServerValue.TIMESTAMP
+      createdAt: serverTimestamp()
     };
-        firebase.database().ref('messages').push(messageObject);
-        setNewMessage('');
+    push(ref(database, 'messages'), messageObject);
+    setNewMessage('');
   };
 
   const handleInputChange = (e) => {
@@ -67,35 +96,35 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        <h1>Chat Room</h1>
-        {user ? (
-          <button onClick={handleSignOut}>Sign Out</button>
-        ) : (
-          <button onClick={handleSignIn}>Sign In with Google</button>
-        )}
-      </header>
-      <div className="container">
-        {user ? (
-          <div>
-            <form onSubmit={handleSubmit}>
-              <input type="text" value={newMessage} onChange={handleInputChange} placeholder="Type a message..." />
-              <button type="submit">Send</button>
-            </form>
-            <ul>
-              {messages.map((message) => (
-                <li key={message.createdAt}>
-                  <strong>{message.user.displayName}</strong>: {message.text}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          <p>Please sign in to view and send messages.</p>
-        )}
-      </div>
+    <header>
+    <h1>Chat Room</h1>
+    {user ? (
+    <button onClick={handleSignOut}>Sign Out</button>
+    ) : (
+    <button onClick={handleSignIn}>Sign In with Google</button>
+    )}
+    </header>
+    <div className="container">
+    {user ? (
+    <div>
+    <form onSubmit={handleSubmit}>
+    <input type="text" value={newMessage} onChange={handleInputChange} placeholder="Type a message..." />
+    <button type="submit">Send</button>
+    </form>
+    <ul>
+    {messages.map((message) => (
+    <li key={message.createdAt}>
+    <strong>{message.user.displayName}</strong>: {message.text}
+    </li>
+    ))}
+    </ul>
     </div>
-  );
-}
-
-export default App;
+    ) : (
+    <p>Please sign in to view and send messages.</p>
+    )}
+    </div>
+    </div>
+    );
+    }
+    
+    export default App;
